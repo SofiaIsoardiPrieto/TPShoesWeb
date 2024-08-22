9e65cb997208dd9625d7be693d05e1af72db0373
Sofía Isoardi Prieto <sofia1_chofi@hotmail.com>
2024-08-22 15:14:06 -0300

    Controller Shoe Index de Shoe

---
+++ TPShoes.Web/Controllers/ShoeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TPShoes.Servicios.Interfaces;
+
+namespace TPShoes.Web.Controllers
+{
+    public class ShoeController : Controller
+    {
+        private readonly IShoesServicio _servicio;
+
+        public ShoeController(IShoesServicio servicio)
+        {
+            _servicio = servicio;
+        }
+
+        public IActionResult Index()
+        {
+            var shoeLista = _servicio.GetLista();
+            return View(shoeLista);
+        }
+    }
+}
