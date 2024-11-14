3cabd94893af9ba1c3c8de28a5e59bcf5395a9ed
Sofía Isoardi Prieto <sofia1_chofi@hotmail.com>
2024-11-13 22:28:49 -0300

    Nuevo de Size anda!!

--- TPShoes.Entidades/ViewModels/SizeShoe/SizeShoeEditVm.cs
+++ TPShoes.Entidades/ViewModels/SizeShoe/SizeShoeEditVm.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace TPShoes.Entidades.ViewModels.SizeShoe
 {
     public class SizeShoeEditVm
     {
         public int SizeShoeId { get; set; }
+
+        [DisplayName("Shoe")]
         public int ShoeId { get; set; }
+
+        [DisplayName("Size")]
         public int SizeId { get; set; }
         public int Stock { get; set; }
 
